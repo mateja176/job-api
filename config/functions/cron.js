7dc2be6a0db9749d6868079069a0e7ddbf8633ad
mateja <mateja176@gmail.com>
2020-04-18 13:38:56 +0200

    feat(config/functions): lists react developer jobs in belgrade

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,4 +1,5 @@
-'use strict';
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 /**
  * Cron config that gives you an opportunity
@@ -15,7 +16,41 @@ module.exports = {
    * Simple example.
    * Every monday at 1am.
    */
-  '*/1 * * * *': (date) => {
-    console.log(`${date} : y super cron task!\n`);
+  '*/1 * * * *': async () => {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto(
+      'https://www.linkedin.com/jobs/search/?distance=50&f_TP=1&geoId=105353691&keywords=react&location=Belgrade%2C%20Centralna%20Srbija%2C%20Serbia',
+    );
+    let content = await page.content();
+
+    // 1 - Load the HTML
+    const $ = cheerio.load(content);
+
+    // 2 - Select the HTML element you need
+    // For the tutorial case, we need to select the list of jobs and for each element, we will
+    // create a new job object to store it in the database with Strapi.
+    $('li.result-card.job-result-card').each((i, el) => {
+      if (Array.isArray(el.children)) {
+        const job = {
+          title: el.children[0].children[0].children[0].data,
+          linkedinUrl: el.children[0].attribs.href,
+          companyName:
+            el.children[2].children[1].children[0].data ||
+            el.children[2].children[1].children[0].children[0].data,
+          descriptionSnippet:
+            el.children[2].children[2].children[1].children[0].data,
+          timeFromNow: el.children[2].children[2].children[2].children[0].data,
+        };
+
+        // 4 - Store the job with the Strapi global.
+        strapi.services.job.create(job);
+      }
+    });
+
+    // 5 - Close the browser
+    browser.close();
   },
 };
